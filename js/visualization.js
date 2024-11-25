2a66211d037f650b02c20d7e8e6307e80f13c8a2
rscassellati <rscassellati@brandeis.edu>
2024-11-24 22:12:42 -0500

    Created scatter plot with data from old assignment

--- js/visualization.js
+++ js/visualization.js
@@ -1,7 +1,35 @@
 // Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
+// variables and prevent race conditions
 ((() => {
 
-  console.log("Hello, world!");
+  // Load the data from a json file (you can make these using
+  // JSON.stringify(YOUR_OBJECT), just remove the surrounding "")
+  d3.json("data/texas.json", (data) => {
+    console.log(data)
+    // General event type for selections, used by d3-dispatch
+    // https://github.com/d3/d3-dispatch
+    const dispatchString = "selectionUpdated";
+
+    // Create a scatterplot given x and y attributes, labels, offsets; 
+    // a dispatcher (d3-dispatch) for selection events; 
+    // a div id selector to put our svg in; and the data to use.
+    let spUnemployMurder = scatterplot()
+      .x(d => d.unemployment)
+      .xLabel("UNEMPLOYMENT RATE")
+      .y(d => d.murder)
+      .yLabel("MURDER RATE IN STATE PER 100000")
+      .yLabelOffset(150)
+      .selectionDispatcher(d3.dispatch(dispatchString))
+      ("#scatterplot", data);
+
+    // When the scatterplot selection is updated via brushing, 
+    // tell the line chart to update it's selection (linking)
+    spUnemployMurder.selectionDispatcher().on(dispatchString, function(selectedData) {
+      //lcYearPoverty.updateSelection(selectedData);
+      //tableData.updateSelection(selectedData);
+      // ADD CODE TO HAVE TABLE UPDATE ITS SELECTION AS WELL
+    });
+
+  });
 
 })());
