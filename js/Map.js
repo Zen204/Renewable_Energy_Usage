f7fa0f0a1be82d738fc1a9189c26d2dfb9aec324
Zen <145291634+Zen204@users.noreply.github.com>
2024-12-15 21:57:32 -0500

    Update Map.js

--- js/Map.js
+++ js/Map.js
@@ -77,12 +77,6 @@ function map(dataset) {
     d3.select(this)
         .style("opacity", 1)
         .style("stroke-width", 3);
-    
-    // Check if the country name exists in the energy data
-    const countryName = d.properties.name;
-    if (energyData[countryName]) {
-        updatePieChart(countryName); // Update the pie chart with the hovered country data
-    }
 
     if (d.mapSelected == false || d.mapSelected == undefined){
         d3.select(this).style("stroke", "pink");
@@ -123,6 +117,11 @@ function map(dataset) {
                 updateBarChart(selectedCountries);
                 updatePieChartWithAverages(selectedCountries, countries.features);
                 // END MAYA EDIT
+                  // Check if the country name exists in the energy data
+    const countryName = d.properties.name;
+    if (energyData[countryName]) {
+        updatePieChart(countryName); // Update the pie chart with the hovered country data
+    }
             });
 
         svg.append("path")
